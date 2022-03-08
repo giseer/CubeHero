7a2747115c4b1d9a2de33ff9a4289255eba79d6a
giseer <sergireb2010@hotmail.com>
2022-03-08 14:54:47 +0100

    PlayerMovement

--- CubeHero/Assets/Scripts/PlayerMovement.cs
+++ CubeHero/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,69 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Variables Movimiento")] 
+    [SerializeField] private float speed;
+    
+    private Rigidbody2D _rigidbody2D;
+    
+    private Vector2 _movementDirection;
+    private Vector2 _input;
+
+
+    private void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Movement();
+    }
+
+    private void FixedUpdate()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Este metodo contiene la logica del movimiento del personaje
+    /// </summary>
+    private void Movement()
     {
+        _input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         
+        // X
+        if (_input.x > 0.1f)
+        {
+            _movementDirection.x = 1f;
+        }
+        else if (_input.x < 0f)
+        {
+            _movementDirection.x = -1f;
+        }
+        else
+        {
+            _movementDirection.x = 0f;
+        }
+        
+        // Y
+        if (_input.y > 0.1f)
+        {
+            _movementDirection.y = 1f;
+        }
+        else if (_input.y < 0f)
+        {
+            _movementDirection.y = -1f;
+        }
+        else
+        {
+            _movementDirection.y = 0f;
+        }
     }
+
+    private void MovementCalculation()
+    {
+        _rigidbody2D.MovePosition(_rigidbody2D.position + _movementDirection * speed * Time.fixedDeltaTime);
+    }
+    
 }
