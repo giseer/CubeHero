fcbf47b010f16dbba0bc383079ce2d6851a0007f
giseer <sergireb2010@hotmail.com>
2022-04-21 12:01:54 +0200

    InventorySlots2

--- CubeHero/Assets/Scripts/Inventory/InventoryItem.cs
+++ CubeHero/Assets/Scripts/Inventory/InventoryItem.cs
@@ -2,7 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum ItemTypes
+{
+    Gun,
+    Potion,
+    Scroll,
+    Ingredient,
+    Treasure
+}
+
 public class InventoryItem : ScriptableObject
 {
+    [Header("Parameters")] 
+    public string ID;
+    public string Name;
+    public Sprite Icon;
+    [TextArea] public string Description;
+
+    [Header("Information")] 
+    public ItemTypes Type;
+    public bool IsConsumable;
 
 }
